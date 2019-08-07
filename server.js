5fd627ca154ebea0718d5f677cd594839423269f
oliversablove <osablove@gmail.com>
2019-08-07 01:16:41 -0400

    Create Post Schema

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const db = require('./config/database')
 
 const userRouter = require('./routes/user')
 
-const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', '*')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-}
+const cors = require('cors')
+
+// const cors = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', '*')
+//   res.header('Access-Control-Allow-Headers', '*')
+//   next()
+// }
 
 // Middleware
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors)
+app.use(express.urlencoded({ limit: '50mb', extended: true}))
+app.use(express.json({ limit: '50mb' }))
+app.use(cors())
 
 // Routes
 app.use('/user', userRouter)
